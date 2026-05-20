5b5e7d01fa9c81f3809d585d4d5cf031cdcc3c00
MacromDEV <elias@macromautopartes.com>
2026-05-20 15:21:21 -0600

    Corrección sitemap 2

--- sitemap.php
+++ sitemap.php
@@ -1,37 +1,27 @@
 <?php
+// 1. Encendemos los errores de PHP
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once "tv-admin/asset/Clases/dbconectar.php";
 require_once "tv-admin/asset/Clases/ConexionMySQL.php";
 
-header("Content-Type: application/xml; charset=utf-8");
+// 2. COMENTAMOS la cabecera XML temporalmente para poder leer el error
+// header("Content-Type: application/xml; charset=utf-8");
 
 $conn = new HelperMySql($array_principal["server"], $array_principal["user"], $array_principal["pass"], $array_principal["db"]);
 
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+echo "CONEXION EXITOSA... <br>";
 
-echo '<url>';
-echo '<loc>https://macromautopartes.com/</loc>';
-echo '<changefreq>daily</changefreq>';
-echo '<priority>1.0</priority>';
-echo '</url>';
-
+// Revisa si tu tabla realmente se llama 'Producto'
 $sql = "SELECT _id, NewUrlName, FechaModificacion FROM Producto WHERE Publicar = 1 AND stock > 0";
 $res = $conn->query($sql);
 
 if ($res) {
+    echo "CONSULTA EXITOSA... <br>";
     while ($row = $conn->fetch($res)) {
-        $url = "https://macromautopartes.com/?mod=catalogo&amp;opc=detalles&amp;_id=" . $row['_id'] . "-" . $row['NewUrlName'];
-        
-        $fecha = date("Y-m-d", strtotime($row['FechaModificacion']));
-        
-        echo '<url>';
-        echo '<loc>' . $url . '</loc>';
-        echo '<lastmod>' . $fecha . '</lastmod>';
-        echo '<changefreq>weekly</changefreq>';
-        echo '<priority>0.8</priority>';
-        echo '</url>';
+        echo $row['_id'] . "<br>";
     }
 }
-
-echo '</urlset>';
 ?>
