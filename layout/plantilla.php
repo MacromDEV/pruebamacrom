482f90bd0970eaec14518f5e1ea271cfe5d08065
MacromDEV <elias@macromautopartes.com>
2026-05-28 12:33:59 -0600

    Devoluciones nueva vista

--- layout/plantilla.php
+++ layout/plantilla.php
@@ -53,31 +53,38 @@
         <meta property="business:contact_data:postal_code" content="28980">
         <meta property="business:contact_data:country_name" content="Mexico">
         <meta name="theme-color" content="#ffffff">
+        <?php
+            $url_limpia = "https://macromautopartes.com" . strtok($_SERVER["REQUEST_URI"], '?');
+        ?>
+        <link rel="canonical" href="<?php echo $url_limpia; ?>" />
         <script type="application/ld+json">
-        {
-          "@context": "https://schema.org",
-          "@type": "AutoPartsStore",
-          "name": "Macrom Autopartes",
-          "image": "https://macromautopartes.com/images/icons/previwmacrom.png",
-          "@id": "https://macromautopartes.com",
-          "url": "https://macromautopartes.com",
-          "telephone": "+523122682028",
-          "address": {
-            "@type": "PostalAddress",
-            "streetAddress": "Av. Benito Juárez #164 Col. La Gloria",
-            "addressLocality": "Villa de Álvarez",
-            "addressRegion": "Colima",
-            "postalCode": "28980",
-            "addressCountry": "MX"
-          },
-          "sameAs": [
-            "https://www.facebook.com/MacromAutopartes/",
-            "https://www.instagram.com/macromautopartes/",
-            "https://www.tiktok.com/@macromautopartes?lang=en",
-            "https://www.youtube.com/channel/UCbVJJH6AIZJ9lH7fE_2rtwA"
-          ]
-        }
+            {
+            "@context": "https://schema.org",
+            "@type": "AutoPartsStore",
+            "name": "Macrom Autopartes",
+            "logo": "https://macromautopartes.com/images/icons/logo%20original.svg",
+            "image": "https://macromautopartes.com/images/icons/previwmacrom.png",
+            "@id": "https://macromautopartes.com",
+            "url": "https://macromautopartes.com",
+            "telephone": "+523122682028",
+            "priceRange": "$$",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Av. Benito Juárez #164 Col. La Gloria",
+                "addressLocality": "Villa de Álvarez",
+                "addressRegion": "Colima",
+                "postalCode": "28980",
+                "addressCountry": "MX"
+            },
+            "sameAs": [
+                "https://www.facebook.com/MacromAutopartes/",
+                "https://www.instagram.com/macromautopartes/",
+                "https://www.tiktok.com/@macromautopartes?lang=en",
+                "https://www.youtube.com/channel/UCbVJJH6AIZJ9lH7fE_2rtwA"
+            ]
+            }
         </script>
+
         <style>
             [ng\:cloak], [ng-cloak], [data-ng-cloak], [x-ng-cloak], .ng-cloak, .x-ng-cloak {
                 display: none !important;
--- modulo/Devoluciones/Controller.php
+++ modulo/Devoluciones/Controller.php
@@ -2,27 +2,40 @@
 
 (@__DIR__ == '__DIR__') && define('__DIR__',  realpath(dirname(__FILE__)));
 
-function get_template($form='principal'){
+function get_template($form = 'principal'){
     $file = __DIR__.'/Html/Devoluciones_'.$form.'.html';
-    $template = file_get_contents($file);
-    return $template;
+    
+    if (file_exists($file)) {
+        return file_get_contents($file);
+    }
+    
+    return "<!-- Error: No se encontró la plantilla HTML -->";
 }
 
-function retorna_vista($vista,$data=array()){
+function retorna_vista($vista, $data = array()){
+    $html = '';
+    
     switch($vista){
         case 'principal':
             $html = get_template($vista);
-            $html = str_replace("{categoria}", $data["categoria"], $html);
+            if (isset($data["categoria"])) {
+                $html = str_replace("{categoria}", $data["categoria"], $html);
+            }
             break;
-       }
+    }
+    
     print $html;
 }
 
 function principal(){
-    $opc = isset($_GET['opc'])? htmlspecialchars($_GET['opc']):"principal";
+    $opc = isset($_GET['opc']) ? htmlspecialchars($_GET['opc']) : "principal";
+    
     switch($opc){
        case 'principal':
            retorna_vista($opc);
+           break;
+       default:
+           retorna_vista('principal');
            break;
     }
 }
